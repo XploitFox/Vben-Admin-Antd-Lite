a88a9e91a99a8cb7b7c8e30110d451ca5045162e
XploitFox <xploitfox@gmail.com>
2026-07-10 21:57:14 +0800

    首次提交

---
+++ apps/web-antd/src/layouts/auth.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { computed, onBeforeMount } from 'vue';
+
+import { AuthPageLayout, LanguageToggle, ThemeToggle } from '@vben/layouts';
+import { preferences, updatePreferences } from '@vben/preferences';
+
+import { $t } from '#/locales';
+
+const appName = computed(() => preferences.app.name);
+const logo = computed(() => preferences.logo.source);
+const logoDark = computed(() => preferences.logo.sourceDark);
+
+onBeforeMount(() => {
+  updatePreferences({
+    app: {
+      authPageLayout: 'panel-center',
+    },
+  });
+});
+</script>
+
+<template>
+  <AuthPageLayout
+    :app-name="appName"
+    :logo="logo"
+    :logo-dark="logoDark"
+    :page-description="$t('authentication.pageDesc')"
+    :page-title="$t('authentication.pageTitle')"
+  >
+    <template #toolbar>
+      <div
+        class="absolute top-4 right-2 z-10 flex-center rounded-3xl bg-accent px-3 py-1"
+      >
+        <LanguageToggle />
+        <ThemeToggle />
+      </div>
+    </template>
+  </AuthPageLayout>
+</template>
---
+++ internal/lint-configs/oxlint-config/src/configs/ignores.ts
@@ -0,0 +1,17 @@
+﻿import type { OxlintConfig } from 'oxlint';
+
+const ignores: OxlintConfig = {
+  ignorePatterns: [
+    '**/dist/**',
+    '**/node_modules/**',
+    'docs/**',
+    '**/*.json',
+    '**/*.md',
+    '**/*.svg',
+    '**/*.yaml',
+    '**/*.yml',
+  ],
+};
+
+export { ignores };
+
